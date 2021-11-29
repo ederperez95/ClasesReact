201eb6eddf1659d1b3c9483c7f3320d4c26d4be2
Eder Perez Garcia <ederperezgarcia@gmail.com>
2021-11-28 20:10:35 -0500

    Variables en JavaScript

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,33 @@
+import logo1 from 'media/cocktail_dog_animal_15960.png';
+
+function Header(){
+    return (
+    <header>
+            <ul className="navbar">
+
+                <li>
+                    <img src={logo1} alt="imagen" className="logo"/>
+                </li> 
+                <li>
+                    <button className="button mainButton">Nuevo post</button>
+                </li>
+                <li>
+                    <div className="buscar">
+                        <input placeholder="Buscar una raza" type="text"/>
+                        <i className="fas fa-search buscarGenerico iconoBusqueda"></i>
+                    </div>
+                </li>
+
+                <li>
+                    <button className="button secondaryButton">Login</button>
+                </li>
+                <li>
+                    <button className="button mainButton">Registro</button>
+                </li>
+            </ul>
+
+        </header>
+    );
+}
+
+export default Header;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,41 +2,14 @@ import CardRazasPerros from "componentes/CardRazasPerros";
 import 'styles/styles.css';
 import boderCollie from 'media/Border-collie.jpg';
 import rhodesian from 'media/Rodesian.jpg';
-import logo1 from 'media/cocktail_dog_animal_15960.png';
+
 
 
 
 function Index(){
     return (
         <div className="App">
-        <header>
-            <div>
-                Hola mundo esto es un div
-            </div>
-            <ul className="navbar">
-  
-                <li>
-                    <img src={logo1} alt="imagen" className="logo"/>
-                </li> 
-                <li>
-                    <button className="button mainButton">Nuevo post</button>
-                </li>
-                <li>
-                    <div className="buscar">
-                        <input placeholder="Buscar una raza" type="text"/>
-                        <i className="fas fa-search buscarGenerico iconoBusqueda"></i>
-                    </div>
-                </li>
-  
-                <li>
-                    <button className="button secondaryButton">Login</button>
-                </li>
-                <li>
-                    <button className="button mainButton">Registro</button>
-                </li>
-            </ul>
-  
-        </header>
+
         <main>
             <section></section>
             <section>
